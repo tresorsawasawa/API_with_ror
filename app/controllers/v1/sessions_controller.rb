b64b68e8cd1efde7692bcad3e81944758ff4b260
tresorsawasawa <tresorsawasawa@gmail.com>
2022-05-30 00:45:03 +0200

    Setting up Devise Authentication

---
+++ app/controllers/v1/sessions_controller.rb
@@ -0,0 +1,9 @@
+class SessionController < ApplicationController
+  def create
+    
+  end
+
+  def destroy
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   namespace :v1 do
     resources :contacts
+    resources :sessions, only: [:create, :destroy]
   end
 end
